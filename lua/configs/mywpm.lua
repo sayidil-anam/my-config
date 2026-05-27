77f019a94ea967889596d04abb53dde307f5faab
sayidil-anam <hantupetani89@gmail.com>
2026-05-27 13:10:10 +0700

    remove old nvim config duplicate

--- lua/configs/mywpm.lua
+++
@@ -1,9 +0,0 @@
-require("mywpm").setup({
-  notify = false,
-  show_virtual_text = true,
-  follow_cursor = true,
-  virt_wpm_pos = "eol",
-  virt_wpm = function(wpm)
-    return ("👨‍💻 speed: %0.f"):format(wpm)
-  end
-})
--- lua/configs/supermaven.lua
+++
@@ -1,7 +0,0 @@
-require("supermaven-nvim").setup({
-  keymaps = {
-    accept_suggestion = "<Tab>",
-  },
-  ignore_filetypes = {rust = true},
-  log_level = "info",
-})
