9f0c11b4693926adce88d7792ca78ade0c6ca397
sayidil-anam <hantupetani89@gmail.com>
2026-05-27 13:01:46 +0700

    Adding nvim config

---
+++ lua/mappings.lua
@@ -0,0 +1,32 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<C-t>", function()
+  require("minty.shades").open({ border = false })
+end, {})
+
+vim.keymap.set("n", "<C-p>", ":FloatermToggle<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-l>", function()
+  require("triforce").show_profile()
+end, { desc = "tampilkan triforce statsnya" })
+
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,141 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    enabled = false,
+  },
+
+  {
+    "IogaMaster/neocord",
+    event = "VeryLazy",
+    config = function()
+      require "configs.discord"
+    end,
+  },
+  {
+    "nvchad/minty",
+    lazy = true,
+    config = function()
+      require "configs.minty"
+    end,
+  },
+
+  { "nvchad/showkeys", cmd = "ShowkeysToggle", opts = { position = "top-center" } },
+  {
+    "nvzone/typr",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = { "Typr", "TyprStats" },
+  },
+
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.chunk"
+    end
+  },
+
+  { "nvzone/volt",     lazy = true },
+  { "nvzone/menu",     lazy = true },
+
+  {
+    "nvzone/timerly",
+    dependencies = 'nvzone/volt',
+    cmd = "TimerlyToggle",
+    opts = {} -- optional
+  },
+
+  {
+    "nvzone/floaterm",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = "FloatermToggle",
+  },
+
+  { 'Civitasv/cmake-tools.nvim', opts = {} },
+
+  {
+    "slowy07/mywpm.nvim",
+    event = "VeryLazy",
+    config = function()
+      require "configs.mywpm"
+    end
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  {
+    "gisketch/triforce.nvim",
+    dependencies = {
+      "nvzone/volt",
+    },
+    config = function()
+      require "configs.triforce"
+    end,
+  },
+
+  -- todo nvim
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      require "configs.todo"
+    end
+  },
+
+  -- trouble nvim
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    lazy = false,
+    cmd = "Trouble",
+  },
+
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+},
+}
